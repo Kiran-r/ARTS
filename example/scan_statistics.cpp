3734ead8f96dc07c2b3fa3c5605855f8dfb12217
Jesun S Firoz <firo017@puma.local>
2018-05-08 18:19:10 -0700

    Some changes to the code to match the output.

--- example/scan_statistics.cpp
+++ example/scan_statistics.cpp
@@ -21,7 +21,7 @@ char* _file = NULL;
 hiveGuid_t maxReducerGuid = NULL_GUID;
 
 typedef struct {
-  hiveGuid_t aggregateNeighborGuid;
+  hiveGuid_t findIntersectionGuid;
   vertex source;
   unsigned int numNeighbors;
   vertex neighbors[];
@@ -61,97 +61,67 @@ hiveGuid_t maxReducer(u32 paramc, u64 * paramv,
   hiveShutdown();
 }
 
-hiveGuid_t aggregateNeighbors(u32 paramc, u64 * paramv,
+hiveGuid_t findIntersection(u32 paramc, u64 * paramv,
 				     u32 depc, hiveEdtDep_t depv[]) {
-  // std::set<vertex> grandUnion;
-  std::vector<vertex> grandUnion;
-
-  /*first create the grand union with one hop neighbors*/
-  for (u32 rank = 0; rank < depc; rank++) {
-    sourceInfo * localUnion = (sourceInfo *) depv[rank].ptr;
-    // for (u32 i = 0; i < localUnion->numNeighbors; i++) {
-    //   grandUnion.insert(localUnion->neighbors[i]);
-    // }
-    std::set_union(grandUnion.begin(), grandUnion.end(),
-	       &(localUnion->neighbors[0]), 
-	       &(localUnion->neighbors[localUnion->numNeighbors]),
-	       std::back_inserter(grandUnion));
+  u64 sum = 0;
+  for (u64 rank = 0; rank < depc; rank++) {
+    perVertexScanStat * localIntersection = (perVertexScanStat *) depv[rank].ptr;
+    sum +=  localIntersection->scanStat;
   }
 
-  /*Then add immediate neighbors to the grand union*/
+  perVertexScanStat * localIntersection = (perVertexScanStat *) depv[0].ptr;
+  vertex source = (vertex) localIntersection->source;
   vertex* neighbors = NULL;
-  u64 neighbor_cnt = 0;
-  sourceInfo * srcInfo = (sourceInfo *) depv[0].ptr;    
-  vertex source = (vertex) srcInfo->source;
+  u64 neighbor_cnt = 0;    
   getNeighbors(&graph, source, &neighbors, &neighbor_cnt);
-  // for (u32 i = 0; i < neighbor_cnt; i++) {
-  //   grandUnion.insert(neighbors[i]);
-  // }
-  std::set_union(grandUnion.begin(), grandUnion.end(),
-		 neighbors, neighbors + neighbor_cnt,
-		 std::back_inserter(grandUnion));
-  /*insert itself*/
-  grandUnion.push_back(source);
-
-  /*Now retain only unique elements*/
-  std::sort(grandUnion.begin(), grandUnion.end());
-  auto last = std::unique(grandUnion.begin(), grandUnion.end());
-  grandUnion.resize(std::distance(grandUnion.begin(), last));
   
+  sum += neighbor_cnt;
 
   unsigned int dbSize =  sizeof(perVertexScanStat);
   void * ptr = NULL;
   hiveGuid_t dbGuid = hiveDbCreate(&ptr, dbSize, false);
   perVertexScanStat * vertexScanStat = (perVertexScanStat *) ptr;  
   vertexScanStat->source = source;
-  vertexScanStat->scanStat = grandUnion.size();
-  std::cout << "Aggregate size for source: " << source << " is " << grandUnion.size() << std::endl;
-  for (std::vector<vertex>::iterator i = grandUnion.begin(); i < grandUnion.end(); i++) {
-    std::cout  << *i << " " ;
-  }
+  vertexScanStat->scanStat = sum;
+  // std::cout << "Aggregate size for source: " << source << " is " << sum << std::endl;
+  // for (std::vector<vertex>::iterator i = grandUnion.begin(); i < grandUnion.end(); i++) {
+  //   std::cout  << *i << " " ;
+  // }
   std::cout << std::endl;
-  hiveSignalEdt(maxReducerGuid, dbGuid, source, DB_MODE_PIN);
+  hiveSignalEdt(maxReducerGuid, dbGuid, source, DB_MODE_ONCE_LOCAL);
 }
 
 hiveGuid_t visitOneHopNeighborOnRank(u32 paramc, u64 * paramv,
 				     u32 depc, hiveEdtDep_t depv[]) {
   sourceInfo * srcInfo = (sourceInfo *) depv[0].ptr;
   vertex* oneHopNeighbor;
-  // std::set<vertex> localUnion;
-  std::vector<vertex> localUnion;
-  for (unsigned int i = 0; i <srcInfo->numNeighbors; i++) {
+  vertex* immediateNeighbors = srcInfo->neighbors;
+  // std::set<vertex> localIntersection;
+  std::vector<vertex> localIntersection;
+  for (unsigned int i = 0; i < srcInfo->numNeighbors; i++) {
     vertex current_neighbor = (vertex) srcInfo->neighbors[i];
-    std::cout << "Current_neighbor: " << current_neighbor << " of  source "  <<  srcInfo->source <<  std::endl;
-    vertex* oneHopNeighbors = NULL;
-    u64 neighbor_cnt = 0;
-    getNeighbors(&graph, current_neighbor, &oneHopNeighbors, &neighbor_cnt);
-    for (unsigned int j = 0; j < neighbor_cnt; j++) {
-      std::cout << "One-hop neighbor for " <<  srcInfo->source << " is: " << oneHopNeighbors[j] << std::endl;
+    if (getOwner(current_neighbor, &distribution) == hiveGetCurrentNode()) {
+      std::cout << "Current_neighbor: " << current_neighbor << " of  source "  <<  srcInfo->source <<  std::endl;
+      vertex* oneHopNeighbors = NULL;
+      u64 neighbor_cnt = 0;
+      getNeighbors(&graph, current_neighbor, &oneHopNeighbors, &neighbor_cnt);
+      for (unsigned int j = 0; j < neighbor_cnt; j++) {
+	std::cout << "One-hop neighbor for " <<  srcInfo->source << " is: " << oneHopNeighbors[j] << std::endl;
+      }
+      std::set_intersection(immediateNeighbors, 
+			    immediateNeighbors + srcInfo->numNeighbors, 
+			    oneHopNeighbors, oneHopNeighbors + neighbor_cnt,
+			    std::back_inserter(localIntersection));
     }
-    std::set_union(localUnion.begin(), localUnion.end(), 
-		   oneHopNeighbors, oneHopNeighbors + neighbor_cnt,
-		   std::back_inserter(localUnion));
-    // for (unsigned int j = 0; j < neighbor_cnt; j++) {
-    //   localUnion.insert(oneHopNeighbors[j]);
-    // }
   }
 
-  /*Now retain only unique elements*/
-  std::sort(localUnion.begin(), localUnion.end());
-  auto last = std::unique(localUnion.begin(), localUnion.end());
-  localUnion.resize(std::distance(localUnion.begin(), last));
-
-  // /*Not sure about data serialization.*/
-  // create  a db containing the local union
-  unsigned int dbSize = sizeof(sourceInfo) + localUnion.size() * sizeof(vertex);
+  unsigned int dbSize =  sizeof(perVertexScanStat);
   void * ptr = NULL;
   hiveGuid_t dbGuid = hiveDbCreate(&ptr, dbSize, false);
-  sourceInfo * srcAndLocalUnionInfo = (sourceInfo *) ptr;
-  srcAndLocalUnionInfo->source = srcInfo->source;
-  srcAndLocalUnionInfo->numNeighbors = localUnion.size();
-  std::copy(localUnion.begin(), localUnion.end(), srcAndLocalUnionInfo->neighbors);
-  hiveSignalEdt(srcInfo->aggregateNeighborGuid, dbGuid, 
-                hiveGetCurrentNode(), DB_MODE_PIN);
+  perVertexScanStat * vertexScanStat = (perVertexScanStat *) ptr;  
+  vertexScanStat->source = srcInfo->source;
+  vertexScanStat->scanStat = localIntersection.size();
+  hiveSignalEdt(srcInfo->findIntersectionGuid, dbGuid, hiveGetCurrentNode(), DB_MODE_ONCE_LOCAL);
 }
 
 hiveGuid_t visitSource(u32 paramc, u64 * paramv, u32 depc, hiveEdtDep_t depv[]) { 
@@ -160,69 +130,31 @@ hiveGuid_t visitSource(u32 paramc, u64 * paramv, u32 depc, hiveEdtDep_t depv[]) 
   vertex source = (vertex) paramv[0];
   getNeighbors(&graph, source, &neighbors, &neighbor_cnt);
   if (neighbor_cnt) {
-    //  following code written in the pre-cpp  version, so  did not use vectors.
-    // qsort(&neighbors, neighbor_cnt, sizeof(graph_sz_t), compare);
-    int * numNeighborPerRank = (int *) calloc (hiveGetTotalNodes(), sizeof(int));
-
-    std::cout << "Source " << source << " have  " << neighbor_cnt << " neighbors" << std::endl;
-    /*Count how many neighbours per rank*/
-    for (unsigned int i = 0; i < neighbor_cnt; i++) {
-      unsigned int neighborRank = getOwner(neighbors[i], &distribution);
-      numNeighborPerRank[neighborRank] += 1;
-    }
-
-    /*Based on the count, allocate memory for each rank's neighbors*/
-    vertex **neighborsPerRank = (vertex **) malloc (hiveGetTotalNodes() * sizeof (vertex*));
+    /*Now spawn an edt that will wait to get oneHopneighbors from all the ranks in slots and calculate the grand count */
+    hiveGuid_t findIntersectionGuid = hiveEdtCreate(findIntersection, hiveGetCurrentNode(), 0, NULL, hiveGetTotalNodes());
+     /*For each rank, now spawn an edt that will perform an intersection*/
     for (unsigned int i = 0; i < hiveGetTotalNodes(); i++) {
-      neighborsPerRank[i] = (vertex *) malloc (numNeighborPerRank[i] * sizeof (vertex));
-    }
-    int * currentNumNeighborPerRank = (int *) calloc (hiveGetTotalNodes(), sizeof(int));
-
-    /*Gather neighbors per rank*/
-    for (unsigned int i = 0; i < neighbor_cnt; i++) {
-      std:: cout << "Source: " << source << " Neighbor: " << neighbors[i] << std::endl;
-      unsigned int neighborRank = getOwner(neighbors[i], &distribution);
-      int currentNumOfNeighbor =  currentNumNeighborPerRank[neighborRank];
-      neighborsPerRank[neighborRank][currentNumOfNeighbor] = neighbors[i];
-      currentNumNeighborPerRank[neighborRank]++;
-    }
-
-    /*Now spawn an edt that will wait to get oneHopneighbors from all the ranks in slots and calculate the grand union */
-    hiveGuid_t aggregateNeighborsGuid = hiveEdtCreate(aggregateNeighbors, hiveGetCurrentNode(), 0, NULL, hiveGetTotalNodes());
- 
-    /*For each rank, now spawn an edt that will perform local union with all the neighbors first and then send back the local union*/
-    for (unsigned int i = 0; i < hiveGetTotalNodes(); i++) {
-      unsigned int dbSize = sizeof (sourceInfo) + sizeof(vertex) * numNeighborPerRank[i];
+      unsigned int dbSize = sizeof (sourceInfo) + (sizeof(vertex) * neighbor_cnt);
       void * ptr = NULL;
       hiveGuid_t dbGuid = hiveDbCreate(&ptr, dbSize, false);
       sourceInfo * srcInfo = (sourceInfo *) ptr;
-      srcInfo->aggregateNeighborGuid = aggregateNeighborsGuid;
+      srcInfo->findIntersectionGuid = findIntersectionGuid;
       srcInfo->source = source;
-      srcInfo->numNeighbors = numNeighborPerRank[i];
-      memcpy(srcInfo->neighbors, neighborsPerRank[i], sizeof(vertex) * numNeighborPerRank[i]);
+      srcInfo->numNeighbors = neighbor_cnt;//numNeighborPerRank[i];
+      memcpy(&(srcInfo->neighbors), neighbors, sizeof(vertex) * neighbor_cnt);
       /*create the edt to find # one-hop neighbors*/
-      /*Can we use the same guid again and agin?*/
       hiveGuid_t visitOneHopNeighborGuid = hiveEdtCreate(visitOneHopNeighborOnRank, i, 0, NULL, 1);
-      hiveSignalEdt(visitOneHopNeighborGuid, dbGuid, 0, DB_MODE_PIN);
+      hiveSignalEdt(visitOneHopNeighborGuid, dbGuid, 0, DB_MODE_ONCE_LOCAL);
    }
   } else {
     /*signal maxreducer*/
-    /*Grand union only contains source*/
     unsigned int dbSize =  sizeof(perVertexScanStat);
     void * ptr = NULL;
     hiveGuid_t dbGuid = hiveDbCreate(&ptr, dbSize, false);
     perVertexScanStat * vertexScanStat = (perVertexScanStat *) ptr;  
     vertexScanStat->source = source;
     vertexScanStat->scanStat = 1;
-    std::vector<vertex> grandUnion;
-    /*insert itself*/
-    grandUnion.push_back(source);    
-    std::cout << "Aggregate size for source: " << source << " is " << grandUnion.size() << std::endl;
-    for (std::vector<vertex>::iterator i = grandUnion.begin(); i < grandUnion.end(); i++) {
-      std::cout  << *i << " " ;
-    }
-    std::cout << std::endl;
-    hiveSignalEdt(maxReducerGuid, dbGuid, source, DB_MODE_PIN);
+    hiveSignalEdt(maxReducerGuid, dbGuid, source, DB_MODE_ONCE_LOCAL);
   }
 }
 
@@ -237,7 +169,7 @@ void initPerNode(unsigned int nodeId, int argc, char** argv) {
   			    &distribution,
   			    argc,
   			    argv);
-  maxReducerGuid = hiveEdtCreate(maxReducer, 0, 0, NULL, distribution.num_vertices);
+  
 }
 
 /*TODO: How to start parallel vertex scan stat calculation? How to do an efficient max reduction?*/
@@ -246,6 +178,7 @@ void initPerWorker(unsigned int nodeId, unsigned int workerId,
 		   int argc, char** argv) {
   printf("Node %u argc %u\n", nodeId, argc);
   if (!nodeId && !workerId) {
+    maxReducerGuid = hiveEdtCreate(maxReducer, 0, 0, NULL, distribution.num_vertices);
     // hiveGuid_t exitGuid = hiveEdtCreate(exitProgram, 0, 0, NULL, 1);    
     // hiveInitializeAndStartEpoch(exitGuid, 0);
     for (uint64_t i = 0; i < distribution.num_vertices; ++i) {
